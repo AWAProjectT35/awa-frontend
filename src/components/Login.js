07acca8b041e81af433cc24deef7831a18ffc0d0
ziptyr <koulu@debian.localdomain>
2021-11-19 14:47:31 +0200

    ternary operator changed into functions

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,37 +4,44 @@ import { loginFunc } from '../loginFunc';
 import styles from './Login.module.css';
 
 
-const Login = () => {
+const Greeting = ({user}) => {
+    let message;
+    if (user) {
+        console.log(user.username)
+        message = "Hello " + user.username;
+    } else {
+        message = "Hello";
+    }
+
+    return <div className={styles.title}>{message}</div>;
+};
+
+
+const Button = ({user, setUser}) => {
+    if (user) {
+        return (<button className={styles.button} onClick={() => setUser(null)}>
+                Log out
+            </button>
+        )
+    } else {
+        return (<button className={styles.button} onClick={async () => {
+            const user = await loginFunc();
+            setUser(user)}}>
+                Log in
+            </button>
+        )
+    }
+};
+
+
+export default function Login() {
 
     const {user, setUser} = useContext(UserContext);
 
     return (
         <div className={styles.container}>
-            {user ? (
-            <div className={styles.title}>Hello {(user.username)}</div>
-            ) : (
-                <div className={styles.title}>Hello </div>)}
-            
-            {user ? (
-                <button className={styles.button}
-                onClick={() => {
-                    setUser(null);
-                }}
-                >
-                Log out
-                </button>
-            ) : (
-            <button className={styles.button}
-            onClick={async () => {
-                const user = await loginFunc();
-                setUser(user);
-            }}
-            >
-            Log in
-            </button>
-            )}
+            <Greeting user={user} />
+            <Button user={user} setUser={setUser} />
         </div>
     )
 }
-
-export default Login
